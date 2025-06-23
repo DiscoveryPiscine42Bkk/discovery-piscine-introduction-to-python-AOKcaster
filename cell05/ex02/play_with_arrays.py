3804767f68fd88c2c90ad33499fbd9c6c90a3ed2
AOKcaster <oakkyza456@gmail.com>
2025-06-23 16:48:05 +0000

    your message

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+array =  [2, 8, 9, 48, 8, 22, -12, 2] 
+new = []
+print("Original array: ", array)
+for i in range(0,len(array)) :
+    if array[i] > 5 :
+       new.append(array[i] + 2)
+print("New array:",new)
